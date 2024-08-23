c6005afe162d07be3f599191b3475c9593914029
haridevarya <haridevarya182@gmail.com>
2024-08-23 12:23:56 +0000

    Delete React Project Code directory

--- React Project Code/before_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# You can add any necessary setup or cleanup tasks here
-echo "Before installation script execution..."
--- React Project Code/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Remove the previous container
-docker rm react-project  --force
-docker container prune --force
-docker image prune -a --force
-# Start the Docker container with the pulled image
-docker run -d --name react-project -p 80:3000 315819558409.dkr.ecr.us-east-1.amazonaws.com/react-project:latest
